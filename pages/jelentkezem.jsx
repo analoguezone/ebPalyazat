4e4f55ffd3e78173fbb592071e9de2bc220f473e
analoguezone <info@analoguezone.com>
2022-04-19 18:42:04 +0200

    lejart'

--- pages/jelentkezem.jsx
+++ pages/jelentkezem.jsx
@@ -3,6 +3,22 @@ import Layout from "../components/MyLayout";
 import { withTranslation } from "../server/i18n";
 import dynamic from "next/dynamic";
 import "react-datepicker/dist/react-datepicker.css";
+
+import {
+  Box,
+  Stack,
+  HStack,
+  Center,
+  VStack,
+  Text,
+  Divider,
+  Img,
+  Button,
+  Heading,
+  Grid,
+  Link,
+  GridItem,
+} from "@chakra-ui/react";
 const Form = dynamic(() => import("../components/LandingPage/Form"));
 
 const FormWrapper = function (props) {
@@ -12,7 +28,11 @@ const FormWrapper = function (props) {
 
   return (
     <Layout {...props}>
-      <Form />
+      <VStack justifyContent="center" h="90vh" color="#eee" backgroundColor="#323232">
+
+        <Center fontSize="2xl">A jelentkezési határidő lejárt!</Center>
+      </VStack>
+      {/* <Form /> */}
     </Layout>
   );
 };
